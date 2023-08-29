a6642bb0fb01fe0c196bb542085696015af0857e
ArtLevel <algitIT@rambler.ru>
2023-08-29 11:59:08 +0300

    15 lesson complete

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -8,28 +8,33 @@ export default {
 }
 
 export const WithValue = () => {
+	const [value, setValue] = useState('1')
+
 	return (
 		<Select
-			value={'1'}
+			value={value}
 			items={[
 				{ value: '1', title: 'Minsk' },
 				{ value: '2', title: 'Moscow' },
 				{ value: '3', title: 'Kiev' }
 			]}
-			onChange={action('Value changed')}
+			onChange={setValue}
 		/>
 	)
 }
 
 export const WithoutValue = () => {
+	const [value, setValue] = useState(null)
+
 	return (
 		<Select
+			value={value}
 			items={[
 				{ value: '1', title: 'Minsk' },
 				{ value: '2', title: 'Moscow' },
 				{ value: '3', title: 'Kiev' }
 			]}
-			onChange={action('Value changed')}
+			onChange={setValue}
 		/>
 	)
 }
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,3 +1,6 @@
+import s from './Select.module.css'
+import { useState, KeyboardEvent, useEffect } from 'react'
+
 type ItemT = {
 	title: string
 	value: any
@@ -10,14 +13,61 @@ type SelectPT = {
 }
 
 export function Select({ items, value, onChange }: SelectPT) {
+	const [active, setActive] = useState(false)
+	const [hoveredElementValue, setHoveredElementValue] = useState(value)
+
 	const selectedItem = items.find(i => i.value === value)
+	const hoveredItem = items.find(i => i.value === hoveredElementValue)
+
+	useEffect(() => setHoveredElementValue(value), [value])
+
+	const toggleItems = () => setActive(prevState => !prevState)
+	const onItemClick = (value: any) => {
+		onChange(value)
+		toggleItems()
+	}
+
+	const onKeyUp = (e: KeyboardEvent<HTMLDivElement>) => {
+		if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+			for (let i = 0; i < items.length; i++) {
+				if (items[i].value === value) {
+					const pretendentElement =
+						e.key === 'ArrowDown' ? items[i + 1] : items[i - 1]
+
+					if (pretendentElement) {
+						onChange(pretendentElement.value)
+						return
+					}
+				}
+			}
+			if (!selectedItem) onChange(items[0].value)
+		}
+		if (e.key === 'Enter' || e.key === 'Escape') {
+			setActive(false)
+		}
+	}
 
 	return (
-		<div>
-			<h3>{selectedItem && selectedItem.title}</h3>
-			{items.map(i => (
-				<div key={i.value}>{i.title}</div>
-			))}
-		</div>
+		<>
+			<div className={s.select} onKeyUp={onKeyUp} tabIndex={0}>
+				<span className={s.main} onClick={toggleItems}>
+					{selectedItem && selectedItem.title}
+				</span>
+				{active && (
+					<div className={s.items}>
+						{items.map(i => (
+							<div
+								key={i.value}
+								onMouseEnter={() => setHoveredElementValue(i.value)}
+								onClick={() => onItemClick(i.value)}
+								className={`${s.item} ${hoveredItem === i ? s.selected : ''}`}
+							>
+								{i.title}
+							</div>
+						))}
+					</div>
+				)}
+			</div>
+		</>
 	)
 }

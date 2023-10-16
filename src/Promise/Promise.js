03f35f22888857671082ecc64bbd35202e7dc28c
ArtLevel <algitIT@rambler.ru>
2023-10-16 18:35:35 +0300

    Added promise in our file

--- src/Promise/Promise.js
+++ src/Promise/Promise.js
@@ -1,4 +1,24 @@
-// pending
-const promise1 = fetch('https://google.com').then(data => console.log(data))
+// pending fulfilled rejected
 
-console.log('Finish')
+const server = {
+	getData() {
+		return new Promise((res, rej) => {
+			res('Hello')
+		})
+	}
+}
+
+const promise = server.getData()
+
+console.log(promise)
+
+function NewPromise(cb) {
+	const resolve = result => {
+		return { state: 'fulfilled', result }
+	}
+	const reject = err => {
+		return { state: 'rejected', err }
+	}
+
+	cb(resolve, reject)
+}

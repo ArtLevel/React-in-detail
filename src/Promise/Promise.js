4be24c496bfec2c7f8e328a98610ae8922220ce0
ArtLevel <algitIT@rambler.ru>
2023-10-16 20:23:14 +0300

    Lesson is completed

--- src/Promise/Promise.js
+++ src/Promise/Promise.js
@@ -3,22 +3,48 @@
 const server = {
 	getData() {
 		return new Promise((res, rej) => {
-			res('Hello')
+			rej('reject1')
 		})
 	}
 }
 
-const promise = server.getData()
+server
+	.getData()
+	.then(t => t + 'then1')
+	.catch(t => t + 'catch1')
+	.catch(t => t + 'catch2')
+	.then(t => t + 'then2')
+	.finally(t => t + 'finally')
+	.then(t => console.log(t))
 
-console.log(promise)
+//
+// const promise = server.getData()
+//
+// console.log(promise)
+//
+// const a = promise.then(data => {
+// 	console.log(data)
+// })
+//
+// console.log(a, 'fasddfsa')
+//
+// promise.catch(err => {
+// 	console.log(err)
+// })
 
-function NewPromise(cb) {
-	const resolve = result => {
-		return { state: 'fulfilled', result }
-	}
-	const reject = err => {
-		return { state: 'rejected', err }
-	}
+// function NewPromise(cb) {
+// 	const resolve = result => {
+// 		return { state: 'fulfilled', result }
+// 	}
+// 	const reject = err => {
+// 		return { state: 'rejected', err }
+// 	}
+//
+// 	cb(resolve, reject)
+// }
 
-	cb(resolve, reject)
-}
+// const delay = ms =>
+// 	new Promise((res, rej) => setTimeout(() => res('resolved'), ms))
+// delay(1000)
+
+//

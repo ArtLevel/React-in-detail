ebc12d1c4881da5ffff1670d32be99d6c7fe36a3
ArtLevel <algitIT@rambler.ru>
2023-11-13 13:29:07 +0300

    Inheritance

--- src/OOP/01.js
+++ src/OOP/01.js
@@ -48,21 +48,37 @@
 // const u2 = new User('Artem', 'it-inc', new Date(1989, 19, s12))
 
 class User {
+	#name = ''
+
 	constructor(name, site, dob) {
-		this.name = name
+		this.#name = name
 		this.site = site
 		this.dateOfBirth = dob
+		this.counter = 0
+	}
+
+	get name() {
+		return this.#name
+	}
+
+	set name(value) {
+		this.#name = value
 	}
 
 	hello() {
 		console.log(`I am ${this.name} from ${this.site}`)
+		this.counter++
 	}
 }
 
 const u1 = new User('Dimych!', 'it-incubator', new Date(1988, 1, 10))
 const u2 = new User('Artem!', 'an IT atmosphere', new Date(1998, 12, 12))
 
-console.log(u1.hello === u2.hello)
+// u1.hello()
+// u2.hello()
 
-u1.hello()
-u2.hello()
+u1.name = 'Igor'
+
+console.log(u1.name)
+// console.log(u1.counter)
+// console.log(u2.counter)
